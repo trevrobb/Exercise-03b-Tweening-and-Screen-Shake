cbdde83904006a58acd3aea0f0a397fd06df65c1
trevrobb <trevrobb@iu.edu>
2022-02-16 01:06:24 -0500

    Finished

---
+++ Explosion/Explosion.gd
@@ -0,0 +1,15 @@
+extends AnimatedSprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play()
+
+
+func _on_Explosion_animation_finished():
+	queue_free()
